39aed9f393756522734cad276c5f7b5283b3fd53
Joel Clark <joelhc@umich.edu>
2020-11-25 13:12:56 -0500

    Added Date

--- Beaver/Views/Components/TaskGrid.swift
+++ Beaver/Views/Components/TaskGrid.swift
@@ -19,6 +19,17 @@ struct TaskGrid: View {
     
     var body: some View {
         ScrollView {
+            VStack(alignment: .center) {
+                Text(todayWeekDay)
+                    .font(.largeTitle).bold()
+                    .foregroundColor(Color("accentWhite"))
+                    .multilineTextAlignment(.center)
+
+                Text(todayDate)
+                    .foregroundColor(Color("dimWhite"))
+                    .multilineTextAlignment(.center)
+            }.padding(.vertical, 16)
+            
             LazyVGrid(columns: columns, spacing: 8) {
                 ForEach(list, id: \.self) { toDo in
                     CardView(toDo: toDo)
@@ -35,5 +46,20 @@ struct TaskGrid: View {
     
     init(list: [ToDo]) {
         self.list = list
+    }
+    
+    var todayWeekDay: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        let weekDay = dateFormatter.string(from: Date())
+        return weekDay
+    }
+    
+    var todayDate: String {
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+        let currentDateString: String = dateFormatter.string(from: date)
+        return currentDateString
     }
 }
